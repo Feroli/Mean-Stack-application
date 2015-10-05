87ec62d11ca191dd2422b3a8b1232482fb1a1c41
Fernando <javierania21@gmail.com>
2015-10-06 00:20:49 +0100

    configured the express app

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.configure(function() {
-  app.set('views',__dirname+'/server/views');
-  app.set('view engine', 'jade');
-});
+
+app.set('views',__dirname + '/server/views');
+app.set('view engine', 'jade');
+
 
 app.get('*', function (req, res) {
   res.render('index');
 });
 
-app.listen = 3030;
+var port = 3030;
 app.listen(port);
 console.log('Listening on port '+ port+ '...');
