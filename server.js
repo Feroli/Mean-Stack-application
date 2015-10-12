6de12381524d1b384602982dfaad83adde954f56
Fernando <javierania21@gmail.com>
2015-10-12 21:38:36 +0100

    Addingg mongoLab

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 var logger = require('morgan')
 var multer = require('multer');
 var upload = multer(); // for parsing multipart/form-data
-    stylus = require('stylus');
+stylus = require('stylus');
+mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,28 +15,53 @@ function compile(str, path) {
 }
 
 
-app.set('views',__dirname + '/server/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(stylus.middleware(
-  {
-    src: __dirname + '/public',
-    compile: compile
-  }
-));
-app.use(express.static(__dirname +'/public'));
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/partials/:partialPath', function(req, res){
-  res.render('partials/'+ req.params.partialPath);
+// connect to Mongod running server
+if (env === 'development') {
+  mongoose.connect('mongodb://localhost/multivision');
+} else {
+  mongoose.connect('mongodb://fernando:multivision@ds035333.mongolab.com:35333/multivision');
+
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db-opened');
 });
 
-app.get('*', function (req, res) {
-  res.render('index');
+//creating schema
+var messageSchema = mongoose.Schema({
+  message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+  res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 });
 
 var port = 3030;
 app.listen(port);
-console.log('Listening on port '+ port+ '...');
+console.log('Listening on port ' + port + '...');
